7efe6af97b9ca3df84188f391401f48378cb8142
Mark Ortiz <ortizmark@hotmail.com>
2021-10-23 01:11:38 -0700

    Added Repositories

---
+++ BlogLab.Models/Blog/BlogCreate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Models.Blog
+{
+    public class BlogCreate
+    {
+        public int BlogId { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [MinLength(10, ErrorMessage = "Min length is 10 characters")]
+        [MaxLength(50, ErrorMessage = "Max length is 50 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Content is required")]
+        [MinLength(300, ErrorMessage = "Min length is 300 characters")]
+        [MaxLength(3000, ErrorMessage = "Max length is 3000 characters")]
+        public string Content { get; set; }
+        public int? PhotoId { get; set; }
+
+    }
+}
---
+++ BlogLab.Models/BlogComment/BlogCommentCreate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Models.BlogComment
+{
+    public class BlogCommentCreate
+    {
+        public int BlogCommentId { get; set; }
+        public int? ParentBlogCommentId { get; set; }
+        public int BlogId { get; set; }
+
+        [Required(ErrorMessage = "Content is required")]
+        [MinLength(10, ErrorMessage = "Min length is 10 characters")]
+        [MaxLength(300, ErrorMessage = "Max length is 300 characters")]
+        public string Content { get; set; }
+
+    }
+
+}
